028174815c25ada9530d258586ae69144dac1ddc
ino yang popper <ino.yang.p@gmail.com>
2025-01-20 22:46:03 -0800

    created Length Lab app

---
+++ Length Lab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Length Lab
+//
+//  Created by Ino Yang Popper on 1/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Length Lab/Length_LabApp.swift
@@ -0,0 +1,17 @@
+//
+//  Length_LabApp.swift
+//  Length Lab
+//
+//  Created by Ino Yang Popper on 1/20/25.
+//
+
+import SwiftUI
+
+@main
+struct Length_LabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
